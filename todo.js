6436205aa2f4849c0c4f2d560a51d9ef36981a09
Adv-2005 <101045521+Adv-2005@users.noreply.github.com>
2024-09-07 13:20:00 +0530

    major changes

--- todo.js
+++ todo.js
@@ -1,93 +1,37 @@
-// let todo = [];
-// let req = prompt("Enter your request:");
-// while (true){
-//     if (req=="quit"){
-//         console.log("quitting app");
-//         break;
-//     }
-//     if (req == "list"){
-//         console.log("------------");
-//         for(let i=0; i<todo.length; i++){
-//             console.log(i+1, todo[i]);
-//         }
-//         console.log("------------");
-//     }
-
-//     else if(req=="add"){
-//         let task= prompt("Enter the task you want to add:");
-//         todo.push(task);
-//         console.log("Task added");
-//     }
-
-//     else if(req=="delete"){
-//         let idx= prompt("Enter the task number you want to delete");
-//         todo.splice(idx-1, 1);
-//         console.log("task deleted");
-//     }
-//     else{
-//         console.log("Error: Invalid request")
-//     }
-//     req = prompt("Enter your request:");
-// }
-
 
 let todo = [];
 let userInput = document.getElementById("userInput").value;
     let outputDiv = document.getElementById("output");
 
-function processRequest() {
-    // let userInput = document.getElementById("userInput").value;
-    // let outputDiv = document.getElementById("output");
-
-    if (userInput == "quit") {
-        outputDiv.innerHTML = "Quitting app";
-    } else if (userInput == "list") {
-        let result = "------------<br>";
-        for (let i = 0; i < todo.length; i++) {
-            result += (i + 1) + " " + todo[i] + "<br>";
-        }
-        result += "------------";
-        outputDiv.innerHTML = result;
-    } else if (userInput == "add") {
-        let task = prompt("Enter the task you want to add:");
-        todo.push(task);
-        outputDiv.innerHTML = "Task added";
-    } else if (userInput == "delete") {
-        let idx = prompt("Enter the task number you want to delete");
-        todo.splice(idx - 1, 1);
-        outputDiv.innerHTML = "Task deleted";
-    } else {
-        outputDiv.innerHTML = "Error: Invalid request";
-    }
-
-    // Clear the input field
-    document.getElementById("userInput").value = "";
-}
-
 function handleKeyPress(event) {
     if (event.key === "Enter") {
-        processRequest();
+        add();
     }
 }
 function add(){
-    // let task = prompt("Enter the task you want to add:");
+    
+    let userInput= document.getElementById('userInput').value
+
         todo.push(userInput);
         outputDiv.innerHTML = "Task added";
 
         list()
+        document.getElementById("userInput").value = "";
 }
 let addbtn=document.getElementById('add')
 addbtn.addEventListener('click',add)
-function list(){
-    let result = "------------<br>";
-        for (let i = 0; i < todo.length; i++) {
-            result += (i + 1) + " " + todo[i] + "<br>";
-        }
-        result += "------------";
-        outputDiv.innerHTML = result;
+function list() {
+    let result = "";
+    
+    for (let i = 0; i < todo.length; i++) {
+        result += "<div class='task'><span class='task-number'>" + (i + 1) + 
+                  "</span><span class='task-text'>" + todo[i] + "</span></div>";
+    }
+    
+    outputDiv.innerHTML = result;
 }
 let listbtn=document.getElementById('list')
-listbtn.addEventListener('click',list)
+
 
 function dlt(){
     let idx = prompt("Enter the task number you want to delete");
@@ -98,6 +42,7 @@ function dlt(){
 }
 let dltbtn=document.getElementById('delete')
 dltbtn.addEventListener('click',dlt)
+
 
 
 
